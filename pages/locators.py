6961f94fce8222439baaf674f9874bb1cbe805de
Aleksandr Nokhrin <a.v.nokhrin@yandex.ru>
2022-08-05 06:28:23 +0500

    Added switching to login page

--- pages/locators.py
+++ pages/locators.py
@@ -6,11 +6,11 @@ class MainPageLocators:
 
 
 class LoginPageLocators:
-    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
+    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form1')
     LOGIN_EMAIL = (By.CSS_SELECTOR, '#login_link')
     LOGIN_PASSWORD = (By.CSS_SELECTOR, '#id_login-password')
 
-    REGISTRATION_FORM = (By.CSS_SELECTOR, '#register_form')
+    REGISTRATION_FORM = (By.CSS_SELECTOR, '#register_form1')
     REGISTRATION_EMAIL = (By.CSS_SELECTOR, '#id_registration-email')
     REGISTRATION_PASSWORD = (By.CSS_SELECTOR, '#id_registration-password1')
     REGISTRATION_CONFIRM_PASSWORD = (By.CSS_SELECTOR, '#id_registration-password2')
