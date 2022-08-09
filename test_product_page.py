7c4acff1580b0d572a6ac8935eeb038396563956
Aleksandr Nokhrin <a.v.nokhrin@yandex.ru>
2022-08-09 06:24:11 +0500

    Added xfail mark to buggy link

--- test_product_page.py
+++ test_product_page.py
@@ -14,6 +14,7 @@ def old_test_guest_can_add_product_to_basket(browser):
     page.open()
     page.should_be_equal_price_and_total()
 
+
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -21,14 +22,14 @@ def old_test_guest_can_add_product_to_basket(browser):
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  pytest.param(
+                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                      marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser=browser, url=link)
     page.open()
-    # browser.delete_all_cookies()
-    # print(page.get_current_url())
     page.should_be_equal_price_and_total()
     page.open()
     page.should_be_equal_product_name_and_added_product_name()
